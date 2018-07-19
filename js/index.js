85af9dec105123673ea0a49c72b9cc15462edb6a
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-18 18:44:01 -0700

    completes first task

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
-// Important: Make sure to add comments to your code to help your future self.
-// Create a copy of your template folder and rename it to week13exercise.
-// Drag this new folder into your 'week13' folder inside your 'codify' folder.
-// When working through these exercises, there are bits and pieces of the instructions that are deliberately left out. You should start thinking about what you THINK you will need in order to accomplish the request.
 // In JS: Create an array of 5 strings. Make sure to capitalize some of the letters in your strings.
+
+var arr = ['asdf', '23rsdfs', 'gfewc', 'danlwuji8u', 'hotdog']
+
 // In JS: Loop through the array. Change each string to lowercase, and then add them to a new array.
+
+
 // In JS: Loop through the new array (all lowercase strings) and log the first and last character of each string to the console.
+
+
 // In JS: Create a variable and store your name in it. Using the split method and a loop, log each letter of your name to the console.
+
+
 // In JS: Using the join method, put your name back together and replace all occurrences of one letter with another. (ie: replace all a's with y's)
+
+
 // In HTML: Create an input and a button.
+
+
 // In JS: Create a function that will get the user's input, test it against the standard regular expression for emails, and log the result in the console.
+
+
 // In JS: Make the function "listen" to the button using addEventListener().
+
+
 // Once you have completed these exercises add them to Github.
